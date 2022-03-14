b84163fe2b09ed8779f96001c5eec09850696d20
hungnguyen1605 <95067355+hungnguyen1605@users.noreply.github.com>
2022-03-14 13:52:12 +0700

    Models_Admin

---
+++ ChangePasswordviewmodel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace webDemo.Areas.Admin.Models
+{
+    public class ChangePasswordviewmodel
+    {
+        [Key]
+        public int AccountId { get; set; }
+        [Display(Name = "Mật khẩu hiện tại")]
+        public string PasswordNow { get; set; }
+        [Display(Name = "Mật khẩu mới")]
+        [Required(ErrorMessage = "Vui lòng nhập Mật khẩu mới")]
+        [MinLength(5, ErrorMessage = "Mật khẩu phải nhập trên 5 ký tự")]
+        public string Password { get; set; }
+        [MinLength(5, ErrorMessage = "Mật khẩu phải nhập trên 5 ký tự")]
+        [Display(Name = "vui lòng nhập lại Mật khẩu mới")]
+        [Compare("Password",ErrorMessage ="Mật khẩu không giống nhau")]
+        public string ConfirmPassword { get; set; }
+    }
+
+
+}
---
+++ LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webDemo.Areas.Admin.Models
+{
+    public class LoignViewModel
+    {
+        [Key]
+        [MaxLength(50)]
+        [Required(ErrorMessage = "Vui lòng nhập email")]
+        [Display(Name = "Địa chỉ Email")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Vui lòng nhập Email")]
+        public string Email { get; set; }
+        [Display(Name = "Mật khẩu")]
+        [Required(ErrorMessage = "Vui lòng nhập Mật khẩu")]
+        [MaxLength(30, ErrorMessage = "Mật khẩu chỉ được nhập 30 ký tự")]
+        public string Password { get; set; }
+
+    }
+}
