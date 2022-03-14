1a7a209f37fc1f1b0c0398525c5556ebf488a8c8
hungnguyen1605 <95067355+hungnguyen1605@users.noreply.github.com>
2022-03-14 14:00:08 +0700

    Add files via upload

---
+++ CacheKeys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webDemo.Enums
+{
+    public enum CacheKeys
+    {
+        Categories,
+        Popular,
+        Social
+
+    }
+}
