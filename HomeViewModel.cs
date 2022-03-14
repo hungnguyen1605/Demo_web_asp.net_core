8bfd6aa7c546f4eec20bc8c43703534ceb2af896
hungnguyen1605 <95067355+hungnguyen1605@users.noreply.github.com>
2022-03-14 14:02:07 +0700

    Add files via upload

---
+++ HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using webDemo.Models;
+namespace webDemo.HomeViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Post> Populars { get; set; }
+        public List<Post> Inspiration { get; set; }
+        public List<Post> Recents { get; set; }
+        public List<Post> Trendings { get; set; }
+        public List<Post> LatestPosts { get; set; }
+        public Post Featured { get; set; }
+    }
+}
