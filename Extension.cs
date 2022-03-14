06470bad5b873e5790422b9b8ef26f276f277589
hungnguyen1605 <95067355+hungnguyen1605@users.noreply.github.com>
2022-03-14 14:00:50 +0700

    Add files via upload

---
+++ Extension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace webDemo.Extension
+{
+    public static class Extension
+    {
+        public static string ToVnd(this double dongia)
+        {
+            return dongia.ToString("#,##0") + "đ";
+        }
+        public static string ToUrlFriendly(this string url)
+        {
+            var result = url.ToLower().Trim();
+            result = Regex.Replace(result, "áàạãảăắằẳẵặâấầậẫẩ", "a");
+            result = Regex.Replace(result, "éèẹẽẻêếềệểễ", "e");
+            result = Regex.Replace(result, "óòõọỏốồỗổộơờớởợỡ", "o");
+            result = Regex.Replace(result, "úùũủụưứửữựừ", "u");
+            result = Regex.Replace(result, "ìíĩỉị", "i");
+            result = Regex.Replace(result, "ỳỹýỷỵ", "y");
+            result = Regex.Replace(result, "đ", "d");
+            result = Regex.Replace(result, "[^a-z0-9-]", "");
+            result = Regex.Replace(result, "(-)+", "-");
+            return result;
+        }
+    }
+}
---
+++ HashMD5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webDemo.Extension
+{
+    public static class HashMD5
+    {
+
+        public static string toMD5(this string password, string saltKey = null)
+        {
+            using (var md5 = MD5.Create())
+            {
+                byte[] data = md5.ComputeHash(Encoding.UTF8.GetBytes(string.Concat(password, saltKey)));
+                StringBuilder sBuilder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+
+                return sBuilder.ToString();
+            }
+        }
+
+    }
+}
