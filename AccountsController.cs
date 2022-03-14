cab522c6badf2256248dd31af081c7ef0f758750
hungnguyen1605 <95067355+hungnguyen1605@users.noreply.github.com>
2022-03-14 13:49:52 +0700

    Controller

---
+++ AccountsController.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using webDemo.Areas.Admin.Models;
+using webDemo.Extension;
+using webDemo.Models;
+
+namespace webDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    //[Authorize(Roles="Admin")]
+
+    public class AccountsController : Controller
+    {
+        private readonly demoContext _context;
+
+        public AccountsController(demoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Accounts
+        public async Task<IActionResult> Index()
+        {
+            var demoContext = _context.Accounts.Include(a => a.Role);
+            return View(await demoContext.ToListAsync());
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("dang-nhap.html",Name ="Login")]
+        public IActionResult Login(string returnUrl)
+        
+        {
+            var TaikhoanID = HttpContext.Session.GetString("AccountId");
+            if (TaikhoanID != null) return RedirectToAction("Index", "Home", new { Areas = "Admin" });
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+     
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("dang-nhap.html", Name = "Login")]
+        public async Task<IActionResult> Login(LoignViewModel model, string returnUrl=null)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            Account kh = _context.Accounts.Include(p => p.Role)
+                .SingleOrDefault(p => p.Email.ToLower() == model.Email.ToLower().Trim());
+            if (kh == null)
+            {
+                ViewBag.Error = "Thông tin đăng nhập chưa chính xác";
+                return View(model);
+            }
+            //string pass = (model.Password.Trim() + kh.Salt.Trim()).toMD5();
+            if (kh.Password.Trim() != model.Password)
+            {
+                ViewBag.Error = "Thông tin đăng nhập chưa chính xác";
+                return View(model);
+            }
+            kh.LastLogin = DateTime.Now;
+            _context.Update(kh);
+            await _context.SaveChangesAsync();
+            var TaikhoanID = HttpContext.Session.GetString("AccountId");
+            HttpContext.Session.SetString("AccountId", kh.AccountId.ToString());
+            var UserClaims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name,kh?.FullName),
+                        new Claim(ClaimTypes.Email,kh?.Email),
+                        new Claim("AccountId",kh?.AccountId.ToString()),
+                       
+
+                    };
+            var roles = _context.Roles.Where(n => n.RoleId == kh.RoleId).ToList();
+            foreach (var item in roles)
+            {
+                UserClaims.Add(new Claim(ClaimTypes.Role,item.RoleName));
+                
+            }
+            var grandmaIdentity = new ClaimsIdentity(UserClaims, "User Identity");
+            var userPrincipal = new ClaimsPrincipal(new[] { grandmaIdentity });
+            await HttpContext.SignInAsync(userPrincipal);
+
+            return Redirect("/Admin/Home/index");
+        }
+        // GET: Admin/Accounts/Details/5
+    
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts
+                .Include(a => a.Role)
+                .FirstOrDefaultAsync(m => m.AccountId == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return View(account);
+        }
+
+        // GET: Admin/Accounts/Create
+        public IActionResult Create()
+        {
+            ViewData["RoleId"] = new SelectList(_context.Roles, "RoleId", "RoleDescription");
+            return View();
+        }
+
+        // POST: Admin/Accounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AccountId,FullName,Email,Phone,Password,Salt,Active,CreatedAt,RoleId,LastLogin")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+            
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoleId"] = new SelectList(_context.Roles, "RoleId", "RoleDescription", account.RoleId);
+            return View(account);
+        }
+
+        // GET: Admin/Accounts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts.FindAsync(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            ViewData["RoleId"] = new SelectList(_context.Roles, "RoleId", "RoleDescription", account.RoleId);
+            return View(account);
+        }
+
+        // POST: Admin/Accounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AccountId,FullName,Email,Phone,Password,Salt,Active,CreatedAt,RoleId,LastLogin")] Account account)
+        {
+            if (id != account.AccountId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(account);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AccountExists(account.AccountId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoleId"] = new SelectList(_context.Roles, "RoleId", "RoleDescription", account.RoleId);
+            return View(account);
+        }
+
+        // GET: Admin/Accounts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts
+                .Include(a => a.Role)
+                .FirstOrDefaultAsync(m => m.AccountId == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return View(account);
+        }
+
+        // POST: Admin/Accounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var account = await _context.Accounts.FindAsync(id);
+            _context.Accounts.Remove(account);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AccountExists(int id)
+        {
+            return _context.Accounts.Any(e => e.AccountId == id);
+        }
+    }
+}
---
+++ PostsController.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using webDemo.Helpers;
+using webDemo.Models;
+
+namespace webDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PostsController : Controller
+    {
+        private readonly demoContext _context;
+
+        public PostsController(demoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Posts
+        public IActionResult Index()
+        {
+            if (!User.Identity.IsAuthenticated) Response.Redirect("/dang-nhap.html");
+            var taikhoanId = HttpContext.Session.GetString("AccountId");
+            if (taikhoanId == null) return RedirectToAction("Login", "Accounts", new { Areas = "Admin" });
+            var Account = _context.Accounts.AsNoTracking().FirstOrDefault(x => x.AccountId == int.Parse(taikhoanId));
+            if (Account == null) return NotFound();
+            var data = _context.Posts.ToList();
+            List<Post> Ispost = new List<Post>();
+            if (Account.RoleId == 3)
+            {
+                Ispost = _context.Posts.Include(p => p.Account).Include(p => p.Cat).OrderByDescending(x => x.CatId).ToList();
+            }
+            else
+            {
+                Ispost = _context.Posts.Include(p => p.Account).Include(p => p.Cat)
+                 .Where(x => x.AccountId == Account.AccountId)
+                .OrderByDescending(x => x.CatId).ToList();
+            }
+
+
+            return View(Ispost);
+        }
+
+        // GET: Admin/Posts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts
+                .Include(p => p.Account)
+                .Include(p => p.Cat)
+                .FirstOrDefaultAsync(m => m.PostId == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // GET: Admin/Posts/Create
+        public IActionResult Create()
+        {
+            if (!User.Identity.IsAuthenticated) Response.Redirect("/dang-nhap.html");
+            var taikhoanId = HttpContext.Session.GetString("AccountId");
+            if (taikhoanId == null) return RedirectToAction("Login", "Accounts", new { Areas = "Admin" });
+            ViewData["Danhmuc"] = new SelectList(_context.Categories, "CatName", "CatId");
+            return View();
+        }
+
+        // POST: Admin/Posts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PostId,Title,Scontents,Contents,Thumb,Published,Alias,CreatedAt,AccountId,Author,Tag,CatId,IsHot,IsNewfeed,Views,MetaKey,MetaDesc")] Post post, Microsoft.AspNetCore.Http.IFormFile fThumb)
+        {
+            if (!User.Identity.IsAuthenticated) Response.Redirect("/dang-nhap.html");
+            var taikhoanId = HttpContext.Session.GetString("AccountId");
+            if (taikhoanId == null) return RedirectToAction("Login", "Accounts", new { Areas = "Admin" });
+            var accounts = _context.Accounts.AsNoTracking().FirstOrDefault(x => x.AccountId == int.Parse(taikhoanId));
+            if (accounts == null) return NotFound();
+            post.Alias = Utilities.SEOURL(post.Title);
+            if (ModelState.IsValid)
+            {
+                post.AccountId = accounts.AccountId;
+                post.Author = accounts.FullName;
+                if (post.CatId == null) post.CatId = 15;
+                post.CreatedAt = DateTime.Now;
+                post.Alias = Utilities.SEOURL(post.Title);
+                post.Views = 0;
+                if (fThumb != null)
+                {
+                    //string extension = Path.GetExtension(fThumb.FileName);
+                    post.Thumb = Utilities.UploadFileToFolder(fThumb, "news");
+                }
+                _context.Add(post);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AccountId"] = new SelectList(_context.Accounts, "AccountId", "AccountId", post.AccountId);
+            ViewData["Danhmuc"] = new SelectList(_context.Categories, "CatId", "CatName", post.CatId);
+            return View(post);
+        }
+
+        // GET: Admin/Posts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+        
+            var post = await _context.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            ViewData["AccountId"] = new SelectList(_context.Accounts, "AccountId", "AccountId", post.AccountId);
+            ViewData["Danhmuc"] = new SelectList(_context.Categories, "CatId", "CatName", post.CatId);
+            return View(post);
+        }
+
+        // POST: Admin/Posts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PostId,Title,Scontents,Contents,Thumb,Published,Alias,CreatedAt,AccountId,Author,Tag,CatId,IsHot,IsNewfeed,Views,MetaKey,MetaDesc")] Post post, Microsoft.AspNetCore.Http.IFormFile fThumb)
+        {
+            if (id != post.PostId)
+            {
+                return NotFound();
+            }
+            if (!User.Identity.IsAuthenticated) Response.Redirect("/dang-nhap.html");
+            var taikhoanId = HttpContext.Session.GetString("AccountId");
+            if (taikhoanId == null) return RedirectToAction("Login", "Accounts", new { Areas = "Admin" });
+            var accounts = _context.Accounts.AsNoTracking().FirstOrDefault(x => x.AccountId == int.Parse(taikhoanId));
+            if (accounts == null) return NotFound();
+            if (accounts.RoleId != 3)
+            {
+                if (post.AccountId != accounts.AccountId) return RedirectToAction(nameof(Index));
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (fThumb != null)
+                    {
+                        //string extension = Path.GetExtension(fThumb.FileName);
+                        post.Thumb = Utilities.UploadFileToFolder(fThumb, "news");
+                    }
+                    post.Alias = Utilities.SEOURL(post.Title);
+                    post.AccountId = accounts.AccountId;
+                    post.Author = accounts.FullName;
+                    _context.Update(post);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PostExists(post.PostId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AccountId"] = new SelectList(_context.Accounts, "AccountId", "AccountId", post.AccountId);
+            ViewData["Danhmuc"] = new SelectList(_context.Categories, "CatId", "CatName", post.CatId);
+            return View(post);
+        }
+
+        // GET: Admin/Posts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts
+                .Include(p => p.Account)
+                .Include(p => p.Cat)
+                .FirstOrDefaultAsync(m => m.PostId == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // POST: Admin/Posts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+            _context.Posts.Remove(post);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PostExists(int id)
+        {
+            return _context.Posts.Any(e => e.PostId == id);
+        }
+    }
+}
